5e6e5c3d67c630023e0a55502fe00ae690811995
Cosme <comeoes97@gmail.com>
2016-12-14 21:33:08 -0700

    posting

---
+++ bot1.js
@@ -0,0 +1,122 @@
+//seting up fb api wrapper
+var FB = require('fb');
+FB.setAccessToken('EAAbJ6X4F5c8BAGnz4whsi158bVbfsHBC6mGCDxD6p3iCDo2e98ZCppSmSOrsQszzHcAkRTbb8dNFQBAz60fyQVLI2auly8ZAps92laBeq55dvVwypBqxs7JHw11RpFLwCzqQNbhlu8rPUPD7378FvSWdFQNkEZD');
+
+
+//seting oup reddit api wrapper
+'use strict';
+const snoowrap = require('snoowrap');
+
+const r = new snoowrap({
+	userAgent: 'redditposter',
+  	clientId: 'Cqx8SkZO8ppXIQ',
+  	clientSecret: 'Zgf9SGtSCJmGvLLz4OYqV37KPcQ',
+  	username: 'Pm_happy_thoughts',
+  	password: 'omarsito1'
+});
+
+//photoshopbattes not jet implemented
+/*r.getSubreddit('photoshopbattles').getHot().then(function(submitions){
+	var count;
+	for(var i =0;submitions.length;i++){
+		
+		if(!submitions[i].stickied){
+			count=i;
+			break;
+		}
+		console.log('stickied post skiped');
+	}
+	console.log('Photoshopbattles' +'\n' +submitions[count].title+' ' +submitions[count].url);
+	submitions[count].expandReplies([{limit:3}]).then(function(sub){
+		var links= [];
+		links.push(submitions[count].url);
+		for(var i=0;i<3;i++){
+			var comment= sub.comments[1].body;
+			
+			if(comment.indexOf(']')> -1){
+				var link= comment.substring(comment.indexOf("](")+2, comment.indexOf(")",comment.indexOf("](")+1));
+				links.push(link);
+			}else{
+				links.push(comment);
+	
+			}
+		}
+		post(submitions.title,links);
+
+	})
+});*/
+
+setInterval(postFromSubRedditWithUrl, 1000*60*60*9,"videos");
+setInterval(postFormSubRedditWithOutUrl, 1000*60*60*4,"todayilearned");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*7,"funny");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*6,"gifs");
+setInterval(postFormSubRedditWithOutUrl, 1000*60*60*15,"LifeProTips");
+setInterval(postFormSubRedditWithOutUrl, 1000*60*60*4,"Showerthoughts");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*9,"aww");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*6,"pics");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*12,"movies");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*6,"AdviceAnimals");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*5,"AnimalsBeingJerks");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*9,"facepalm");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*4,"thisismylifenow");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*10,"mildlyinteresting");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*13,"comics");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*10,"interestingasfuck");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*11,"Unexpected");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*12,"instant_regret");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*10,"ChildrenFallingOver");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*11,"BikiniBottomTwitter");
+setInterval(postFromSubRedditWithUrl, 1000*60*60*10,"PerfectTiming");
+
+
+
+
+
+
+
+function postFromSubRedditWithUrl(subreddit){
+	r.getSubreddit(subreddit).getHot().then(function(submitions){
+		var count;
+		for(var i =0;submitions.length;i++){
+			if(!submitions[i].stickied && !submitions[i].over_18){
+				count=i;
+				break;
+			}
+			console.log('post stickied or nsfw skiped');
+		}
+		post(submitions[count].title+"\n-by /u/"+submitions[count].author.name,submitions[count].url);
+	});
+}
+
+function postFormSubRedditWithOutUrl(subreddit){
+	r.getSubreddit(subreddit).getHot().then(function(submitions){
+		var count;
+		for(var i =0;submitions.length;i++){
+			
+			if(!submitions[i].stickied && !submitions[i].over_18){
+				count=i;
+				break;
+			}
+			console.log('stickied post skiped');
+		}
+		post(submitions[count].title+"\n-by /u/"+submitions[count].author.name);
+	});
+}
+
+
+
+
+//function to post in fb
+function post(mensaje, link){
+	var body = 'Second test';
+	var foto = 'https://i.redditmedia.com/tdBgH_kXHoTq16v0G3nNu0XYp4udqwq-YKL0rOeM_CI.jpg?w=663&s=1c3e515671fd53c19e4dccbe2d6dc2cc';
+	FB.api('me/feed', 'post', { message: mensaje,link:link}, function (res) {
+	  if(!res || res.error){
+	    console.log(!res ? 'error occurred' : res.error);
+	    return;
+	  }
+	  console.log('Post Id: ' + res.id);
+	});
+}
+
+
